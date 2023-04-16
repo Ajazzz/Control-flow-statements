a1bfed85ee2a8db2478605886a980caa9fc03208
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-16 09:51:38 +0530

    Update zip.py

--- zip.py
+++ zip.py
@@ -1,15 +1,15 @@
-Zip:
+'''Zip:
 The zip function is a built-in function that allows you to iterate over two or more sequences in parallel,
-i.e. iterating over the corresponding elements of each sequence at the same time. 
+i.e. iterating over the corresponding elements of each sequence at the same time.''' 
 
-For example:
+# For example:
 
 names = ["Alice", "Bob", "Charlie"]
 ages = [25, 30, 35]
 for name, age in zip(names, ages):
     print(name, age)
     
-Output:
+# Output:
 
 Alice 25
 Bob 30
